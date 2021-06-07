9576fbc1c8ed34bc905d31f2b84b85e68cf12899
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2021-06-07 15:15:28 +0500

    added config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,28 @@
 import express from "express";
 import { pool } from "./db/pool";
+import { logger } from "./middlewares/logger";
 import { postsRouter } from "./routes/posts";
 import { usersRouter } from "./routes/users";
+import morgan from "morgan";
+import config from "config";
 
 const app = express();
 const PORT = 5000;
 
+// Middleware
 app.use(express.json());
+
+if (app.get("env") === "development") {
+  app.use(morgan("dev"));
+}
+
+// Routers
 app.use(postsRouter);
 app.use(usersRouter);
+
+app.use((req, res) => {
+  res.status(404).send("No page found");
+});
 
 pool
   .connect()
---
+++ server/src/middlewares/logger.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from "express";
+
+export const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log("Headers ", req.headers);
+  console.log("Body ", req.body);
+  next();
+};
